65649997a71af63eeba3f5d8ca228aa725269682
dchirkov <mail.chirkov@gmail.com>
2023-06-20 14:32:22 +0300

    Added ext for install ansible

--- DEPLOY_resources/VMs/dp_vms.ps1
+++ DEPLOY_resources/VMs/dp_vms.ps1
@@ -2,12 +2,11 @@ $RG = 'RG_AzProject_VMs'
 $VMName = 'VM-ANS-1'
 $Location = 'East US 2'
 $MasterSize = 'Standard_B1s'
-#$SlaveSize = 'Standard_B1ls'
-#$NumberOfClients = Read-Host "Enter the number of slave VMs"
+$SlaveSize = 'Standard_B1ls'
+$NumberOfClients = Read-Host "Enter the number of slave VMs"
 $Template = '.\DATA\ARM_Templates\VMs\basic_resources.json'
 $TemplateMVM = '.\DATA\ARM_Templates\VMs\master_vms.json'
-#$TemplateCVM = '.\DATA\ARM_Templates\VMs\slave_vms.json'
-$TemplateExtAns = '.\DATA\ARM_Templates\Extensions\ansible.json'
+$TemplateCVM = '.\DATA\ARM_Templates\VMs\slave_vms.json'
 $KeyForServers = Get-Content -Path ".\DATA\Public_keys\manage.txt"
 $KeyForClients = Get-Content -Path ".\DATA\Public_keys\clients.txt"
 
@@ -15,37 +14,36 @@ $KeyForClients = Get-Content -Path ".\DATA\Public_keys\clients.txt"
 New-AzResourceGroup -Name $RG -Location $Location -Force 
 
 #Deploying basic resources
-#New-AzResourceGroupDeployment `
-#  -ResourceGroupName $RG `
-#  -TemplateFile $Template `
-#  -PublicKeyForServers $KeyForServers `
-#  -PublicKeyForClients $KeyForClients
-
-#Deploying a VM to manage
-#New-AzResourceGroupDeployment `
-#  -ResourceGroupName $RG `
-#  -TemplateFile $TemplateMVM `
-#  -vmName $VMName `
-#  -vmSize $MasterSize `
-#Installing ansible
-
 New-AzResourceGroupDeployment `
   -ResourceGroupName $RG `
-  -TemplateFile $TemplateExtAns `
-  -vmName $VMName `
+  -TemplateFile $Template `
+  -PublicKeyForServers $KeyForServers `
+  -PublicKeyForClients $KeyForClients
 
-#Set-AzVMExtension `
-#  -VMName $VMName `
-#  -ResourceGroupName $RG `
-#  -Name $TemplateExtAns `
-#  -ExtensionType "CustomScriptExtension"
+#Deploying a VM to manage
+New-AzResourceGroupDeployment `
+  -ResourceGroupName $RG `
+  -TemplateFile $TemplateMVM `
+  -vmName $VMName `
+  -vmSize $MasterSize `
+#Installing ansible
+$Params = @{
+  ResourceGroupName  = $RG
+  VMName             = $VMName
+  Name               = 'CustomScript'
+  Publisher          = 'Microsoft.Azure.Extensions'
+  ExtensionType      = 'CustomScript'
+  TypeHandlerVersion = '2.1'
+  Settings          = @{fileUris = @('https://raw.githubusercontent.com/DAChirkov/DevOps_Project/deploy_sh/DATA/ARM_Templates/Extensions/install_ansible.sh'); commandToExecute = 'sudo sh install_ansible.sh'}
+}
+Set-AzVMExtension @Params
 
 #Deploying slave VMs
-#New-AzResourceGroupDeployment `
-#  -ResourceGroupName $RG `
-#  -TemplateFile $TemplateCVM `
-#  -numberOfInstances $NumberOfClients `
-#  -vmSize $SlaveSize
+New-AzResourceGroupDeployment `
+  -ResourceGroupName $RG `
+  -TemplateFile $TemplateCVM `
+  -numberOfInstances $NumberOfClients `
+  -vmSize $SlaveSize
 
 #Show IP-Addresses
 (Get-AzNetworkInterface -ResourceGroupName $RG).IpConfigurations.PrivateIpAddress
