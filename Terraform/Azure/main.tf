250961bc492ef960661b44d29cd8273be1331977
dchirkov <mail.chirkov@gmail.com>
2023-07-09 17:23:30 +0300

    Fixed deploy problems

--- Terraform/Azure/main.tf
+++ Terraform/Azure/main.tf
@@ -17,12 +17,7 @@ provider "azurerm" {
   features {}
 }
 
-data "azurerm_resource_group" "existing" {
-  name = var.resource_group_name
-}
-
 resource "azurerm_resource_group" "rg" {
-  count    = data.azurerm_resource_group.existing ? 0 : 1
   location = var.resource_group_location
   name     = var.resource_group_name
 }
@@ -32,28 +27,24 @@ resource "azurerm_resource_group" "rg" {
 ###############################
 
 resource "azurerm_virtual_network" "vnet" {
-  depends_on          = [data.azurerm_resource_group.existing]
   resource_group_name = var.resource_group_name
   location            = var.resource_group_location
   name                = var.resource_virtual_network
   address_space       = ["10.1.0.0/16"]
 }
 resource "azurerm_subnet" "snet1" {
-  depends_on           = [azurerm_virtual_network.vnet]
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.resource_virtual_network
   name                 = var.resource_subnet1
   address_prefixes     = ["10.1.0.0/24"]
 }
 resource "azurerm_subnet" "snet2" {
-  depends_on           = [azurerm_virtual_network.vnet]
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.resource_virtual_network
   name                 = var.resource_subnet2
   address_prefixes     = ["10.1.1.0/24"]
 }
 resource "azurerm_subnet" "snet3" {
-  depends_on           = [azurerm_virtual_network.vnet]
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.resource_virtual_network
   name                 = var.resource_subnet3
@@ -61,13 +52,11 @@ resource "azurerm_subnet" "snet3" {
 }
 
 resource "azurerm_network_security_group" "main-nsg" {
-  depends_on          = [data.azurerm_resource_group.existing]
   resource_group_name = var.resource_group_name
   location            = var.resource_group_location
   name                = var.resource_main-nsg
 }
 resource "azurerm_network_security_rule" "main-nsg-ssh" {
-  depends_on                  = [azurerm_network_security_group.main-nsg]
   name                        = "SSH"
   resource_group_name         = var.resource_group_name
   network_security_group_name = var.resource_main-nsg
@@ -81,7 +70,6 @@ resource "azurerm_network_security_rule" "main-nsg-ssh" {
   destination_address_prefix  = "10.1.0.4"
 }
 resource "azurerm_network_security_rule" "main-nsg-http" {
-  depends_on                  = [azurerm_network_security_group.main-nsg]
   name                        = "SiteAllow_HTTP"
   resource_group_name         = var.resource_group_name
   network_security_group_name = var.resource_main-nsg
