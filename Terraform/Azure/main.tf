1fb267061c5b47ae62ff91a5711a2ef525d40599
dchirkov <mail.chirkov@gmail.com>
2023-07-14 21:24:56 +0300

    Added module "frontend_vms"

--- Terraform/Azure/main.tf
+++ Terraform/Azure/main.tf
@@ -104,11 +104,21 @@ resource "azurerm_network_security_rule" "nsg_main_http" {
 
 #####################################################
 
-module "vms_for_manage" {
-  source                    = "./modules/1_vms_manage"
+# module "vms_for_manage" {
+#   source                    = "./modules/vms_manage"
+#   resource_group_name       = var.resource_group_name
+#   resource_group_location   = var.resource_group_location
+#   subnet_id                 = azurerm_subnet.snet1.id
+#   network_security_group_id = azurerm_network_security_group.nsg_main.id
+#   public_key                = azurerm_ssh_public_key.ssh_servers_key.public_key
+# }
+
+module "frontend_vms" {
+  source                    = "./modules/vms_frontend"
   resource_group_name       = var.resource_group_name
   resource_group_location   = var.resource_group_location
-  subnet_id                 = azurerm_subnet.snet1.id
+  subnet_id                 = azurerm_subnet.snet2.id
   network_security_group_id = azurerm_network_security_group.nsg_main.id
-  public_key                = azurerm_ssh_public_key.ssh_servers_key.public_key
+  public_key                = azurerm_ssh_public_key.ssh_clients_key.public_key
+  vm_count                  = 1
 }
