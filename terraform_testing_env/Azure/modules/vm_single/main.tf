26fe9cbad4ac282fcc71bcd7887c164d30517e0a
dchirkov <mail.chirkov@gmail.com>
2023-07-16 20:44:19 +0300

    Added Public IP

--- terraform_testing_env/Azure/modules/vm_single/main.tf
+++ terraform_testing_env/Azure/modules/vm_single/main.tf
@@ -2,6 +2,18 @@
 # Deploy VM for test
 #################################################################
 
+# Create public IP
+resource "azurerm_public_ip" "frontend_servers" {
+  count                   = var.vm_count
+  resource_group_name     = var.resource_group_name
+  location                = var.resource_group_location
+  name                    = "${var.frontend_prefix}-${count.index + 1}_PublicIP"
+  allocation_method       = "Dynamic"
+  sku                     = "Basic"
+  domain_name_label       = lower("${var.frontend_prefix}-${count.index + 1}")
+  idle_timeout_in_minutes = 4
+}
+
 # Create network interface
 resource "azurerm_network_interface" "test_server" {
   count               = var.vm_count
@@ -12,6 +24,7 @@ resource "azurerm_network_interface" "test_server" {
     name                          = "${var.test_prefix}-${count.index + 1}_ipconfig1"
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = element(azurerm_public_ip.test_server.*.id, count.index)
   }
 }
 
